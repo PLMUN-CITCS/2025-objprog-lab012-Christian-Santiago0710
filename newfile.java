acd8f501ef68aefad435c359b3b7da0323ba26d2
Christian-Santiago0710 <>
2025-03-19 12:08:37 +0800

    Last Sync: 2025-03-19 04:08:37 (Mobile)

---
+++ newfile.java
@@ -0,0 +1,11 @@
+public class GreetingProgram {
+
+    public static void greet() {
+        System.out.println("Hello from the greet method!");
+    }
+
+    public static void main(String[] args) {
+        greet(); // Call the greet method
+        System.out.println("This is my message!");
+    }
+}
